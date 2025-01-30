36a2519b6bee32a5e88099fc278b4af9c67ea09a
Rohan <shingaderohan96@gmail.com>
2025-01-30 20:30:01 +0530

    Tailwind Css installed & project run successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
+      <div className='text-green-500 text-3xl underline font-bold'>
         <h1> Hello World! </h1>
       </div>
-      
     </>
   )
 }
